03eb871a722d72d1513611d4b63459e50cf37130
Jan Bartonik <bartonik@worklio.cz>
2022-04-08 17:09:25 +0200

    testapollo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
+import Vue from 'vue'
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createProvider } from './vue-apollo'
+
+new Vue({
+    apolloProvider: createProvider(),
+    render: h => h(App)
+}).$mount('#app');
 
 createApp(App).mount('#app')
